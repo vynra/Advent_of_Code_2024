13a58fa4071ab4fcbb54b999f5e99da0bec83ae3
vynra <ladbrubaker@gmail.com>
2024-12-03 08:21:28 -0700

    both parts together

--- day_3/day_3.go
+++ day_3/day_3.go
@@ -10,6 +10,23 @@ import (
 	"time"
 )
 
+func getSubtotal(result string) int {
+	re := regexp.MustCompile(`mul\(\d{1,3}\,\d{1,3}\)`)
+	muls := re.FindAllString(string(result), -1)
+	var total int = 0
+	for i := 0; i < len(muls); i++ {
+		reint := regexp.MustCompile(`\d{1,3}`)
+		nums := reint.FindAllString(muls[i], -1)
+		// fmt.Printf("%v\n", nums)
+		num1, err := strconv.Atoi(nums[0])
+		num2, err := strconv.Atoi(nums[1])
+		if err == nil {
+			total += num1 * num2
+		}
+	}
+	return total
+}
+
 func main() {
 	start := time.Now()
 	f, err := os.ReadFile("../inputs/day_3.txt")
@@ -25,27 +42,21 @@ func main() {
 		laststart = element[0]
 	}
 	result[len(indices)] = string(f[laststart:])
-	re := regexp.MustCompile(`mul\(\d{1,3}\,\d{1,3}\)`)
-	var total int = 0
+	var total_1 = 0
+	var total_2 int = 0
 	for j := 0; j < len(result); j++ {
 		// fmt.Printf("%s\n", result[j])
 		if !strings.Contains(result[j], "don't") {
-			muls := re.FindAllString(string(result[j]), -1)
-			var num1 int
-			var num2 int
-			for i := 0; i < len(muls); i++ {
-				reint := regexp.MustCompile(`\d{1,3}`)
-				nums := reint.FindAllString(muls[i], -1)
-				// fmt.Printf("%v\n", nums)
-				num1, err = strconv.Atoi(nums[0])
-				num2, err = strconv.Atoi(nums[1])
-				total += num1 * num2
-			}
+			total_1 += getSubtotal(result[j])
+			total_2 += getSubtotal(result[j])
+		} else {
+			total_1 += getSubtotal(result[j])
 		}
 	}
 	// fmt.Printf("%v\n", indices)
 	// fmt.Printf("%s\n", result[1])
-	fmt.Printf("%d\n", total)
+	fmt.Printf("%d\n", total_1)
+	fmt.Printf("%d\n", total_2)
 
 	elapsed := time.Since(start)
 	fmt.Printf("%s\n", elapsed)
