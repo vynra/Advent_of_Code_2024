fda1e13bc80189adf52353cd1b402176b9fc3e64
vynra <ladbrubaker@gmail.com>
2024-12-07 09:28:26 -0700

    day 7 part 1 (I forgot to push part 1)

---
+++ day_7/day_7.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func check_op(goal int, curr_val int, nums []string) bool {
+	var mult_curr_val int = curr_val
+	var add_curr_val int = curr_val
+	var cat_curr_val string
+	// fmt.Printf("%d, %d, %v\n", goal, curr_val, nums)
+	var mult bool
+	var add bool
+	var cat bool
+	if curr_val > goal {
+		return false
+	}
+	if len(nums) == 0 {
+		if curr_val == goal {
+			return true
+		} else {
+			return false
+		}
+	}
+	num, _ := strconv.Atoi(nums[0])
+	mult_curr_val *= num
+	mult = check_op(goal, mult_curr_val, nums[1:])
+	add_curr_val += num
+	add = check_op(goal, add_curr_val, nums[1:])
+	cat_curr_val = strconv.Itoa(curr_val) + nums[0]
+	// fmt.Printf("POST CAT %s\n", cat_curr_val)
+	temp, _ := strconv.Atoi(cat_curr_val)
+	cat = check_op(goal, temp, nums[1:])
+	return mult || add || cat
+}
+
+func main() {
+
+	start := time.Now()
+	f, err := os.Open("../inputs/day_7.txt")
+	if err != nil {
+		log.Fatalf("Failed to open file %s: %v", f.Name(), err)
+	}
+	scanner := bufio.NewScanner(f)
+	var total int = 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		split := strings.Split(line, ":")
+		goal, _ := strconv.Atoi(split[0])
+		split2 := strings.Split(split[1], " ")
+		start_num, _ := strconv.Atoi(split2[1])
+		// fmt.Printf("%v\n", split2[2:])
+		valid := check_op(goal, start_num, split2[2:])
+		if valid {
+			total += goal
+		}
+	}
+	fmt.Printf("%d\n", total)
+	elapsed := time.Since(start)
+	fmt.Printf("%s\n", elapsed)
+}
